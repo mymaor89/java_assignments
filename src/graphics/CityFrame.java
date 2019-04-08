0b8b9dc5b13973da3937aa0217558a2cb2e5416d
Maor Yastskan <mymaor89@gmail.com>
2019-04-08 11:02:49 +0300

    menu working

--- src/graphics/CityFrame.java
+++ src/graphics/CityFrame.java
@@ -8,55 +8,72 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class CityFrame extends JFrame  {
-	
+public class CityFrame extends JFrame {
+
 	private static final int WIDTH = 1024;
 	private static final int HEIGHT = 700;
 	private static int panel_height = HEIGHT;
 	private static int panel_width = WIDTH;
-	//private  CityPanel panel;
-	//private   menuFile;
-	//private  JMenu menuHelp;
-	//private  JMenuItem exitItem,helpItem;
+
+	// private CityPanel panel;
+	// private menuFile;
+	// private JMenu menuHelp;
+	// private JMenuItem exitItem,helpItem;
 
 	public CityFrame(String string) {
 		super(string);
 	}
+
 	public static void main(String[] args) {
 		CityFrame f = new CityFrame("City");
-		f.setSize(WIDTH,HEIGHT);
+		f.setSize(WIDTH, HEIGHT);
 		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		JMenuBar bar = new JMenuBar();
-		JMenu menuFile = new JMenu("File");
-		JMenu menuHelp = new JMenu("Help");
-		JMenuItem exitItem = new JMenu("Exit");
-		JMenuItem helpItem = new JMenu("Help");
-		
-		exitItem.setActionCommand("Exit");
-		helpItem.setActionCommand("Help");
-		menuFile.add(exitItem);
-		menuHelp.add(helpItem);
-		bar.add(menuFile);
-		bar.add(menuHelp);
-		f.setJMenuBar(bar);
-		
-		f.setResizable(false);
-		
-		exitItem.setEnabled(true);
-		exitItem.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("dasfvb");
+		javax.swing.JMenu jMenuFile;
+		javax.swing.JMenu jMenuHelp;
+		javax.swing.JMenuBar jMenuBar1;
+		javax.swing.JMenuItem jMenuItemExit;
+		javax.swing.JMenuItem jMenuItemHelp;
+		jMenuBar1 = new javax.swing.JMenuBar();
+		jMenuFile = new javax.swing.JMenu();
+		jMenuItemExit = new javax.swing.JMenuItem();
+		jMenuItemHelp = new javax.swing.JMenuItem();
+		jMenuHelp = new javax.swing.JMenu();
+		jMenuFile.setText("File");
+		jMenuItemExit.setText("Exit");
+		jMenuItemHelp.setText("Help");
+		jMenuItemExit.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				System.exit(0);
 			}
 		});
-		f.setVisible(true);
-		CityPanel panel = new CityPanel(panel_width,panel_height);
+		jMenuItemHelp.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				System.exit(0);
+			}
+		});
+		jMenuFile.add(jMenuItemExit);
+		jMenuHelp.add(jMenuItemHelp);
+		jMenuBar1.add(jMenuFile);
+		jMenuBar1.add(jMenuHelp);
+
+		f.setJMenuBar(jMenuBar1);
+		/*
+		 * JMenuBar bar = new JMenuBar(); JMenu menuFile = new JMenu("File"); JMenu
+		 * menuHelp = new JMenu("Help"); JMenuItem exitItem = new JMenu("Exit");
+		 * JMenuItem helpItem = new JMenu("Help");
+		 * 
+		 * exitItem.setActionCommand("Exit"); helpItem.setActionCommand("Help");
+		 * exitItem.addActionListener(new java.awt.event.ActionListener() { public void
+		 * actionPerformed(java.awt.event.ActionEvent evt) { System.exit(0); } });
+		 * menuFile.add(exitItem); menuHelp.add(helpItem); bar.add(menuFile);
+		 * bar.add(menuHelp); f.setJMenuBar(bar);
+		 */
+		f.setResizable(false);
+		CityPanel panel = new CityPanel(panel_width, panel_height);
 		f.add(panel);
-		
+		f.pack();
+		f.setVisible(true);
+
 	}
-	
 
 }
